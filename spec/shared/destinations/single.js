59346753e45d9d939e8117d31aff35d246a6bb97
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2018-02-02 15:26:27 +0300

    port [changes](https://github.com/robtweed/ewd-qoper8-express/commit/e7c8c22f304c14c5cd4320db3bf155d664862281) from ewd-qoper8-express

--- spec/shared/destinations/single.js
+++ spec/shared/destinations/single.js
@@ -85,6 +85,162 @@ module.exports = function (boot) {
       });
     },
 
+    whenTokenHasValueUndefined: function () {
+      beforeEach(function (done) {
+        boot(function (_q, _messageObj, _handleResponse) {
+          q = _q;
+          messageObj = _messageObj;
+          handleResponse = _handleResponse;
+
+          done();
+        });
+      });
+
+      it('should call handleMessage with correct arguments', function () {
+        q.microServiceRouter(messageObj, handleResponse);
+
+        expect(q.handleMessage).toHaveBeenCalledTimes(1);
+        expect(q.handleMessage).toHaveBeenCalledWith({
+          type: 'ewd-jwt-updateExpiry',
+          params: {
+            jwt: 'token1',
+            application: 'login-micro-service'
+          }
+        }, jasmine.any(Function));
+      });
+
+      it('should call microservice client #send with correct arguments', function () {
+        /*jshint camelcase: false */
+        var microService = q.u_services.byDestination[destination];
+        /*jshint camelcase: true */
+
+        q.microServiceRouter(messageObj, handleResponse);
+
+        expect(microService.client.send).toHaveBeenCalledTimes(1);
+        expect(microService.client.send).toHaveBeenCalledWith({
+          application: 'login-micro-service',
+          type: 'restRequest',
+          path: '/api/users',
+          pathTemplate: '/path/template',
+          method: 'POST',
+          headers: {
+            'x-foo': 'bar'
+          },
+          params: {
+            type: 'foo'
+          },
+          query: {
+            bar: 'baz'
+          },
+          body: {
+            login: 'johndoe',
+            password: 'secret'
+          },
+          ip: '127.0.0.1',
+          ips: ['client'],
+          token: 'updated-jwt-token',
+          args: {},
+          jwt: true
+        }, jasmine.any(Function));
+      });
+
+      it('should call handleResponse', function () {
+        var responseObj = {
+          foo: 'bar'
+        };
+
+        /*jshint camelcase: false */
+        var microService = q.u_services.byDestination[destination];
+        /*jshint camelcase: true */
+
+        microService.client.send.and.callFake(function (message, cb) {
+          cb(responseObj);
+        });
+
+        q.microServiceRouter(messageObj, handleResponse);
+
+        expect(handleResponse).toHaveBeenCalledWith(responseObj);
+      });
+    },
+
+    whenRouteByPassJwtCheckEnabled: function () {
+      beforeEach(function (done) {
+        boot(function (_q, _messageObj, _handleResponse) {
+          q = _q;
+          messageObj = _messageObj;
+          handleResponse = _handleResponse;
+
+          done();
+        });
+      });
+
+      it('should call handleMessage with correct arguments', function () {
+        q.microServiceRouter(messageObj, handleResponse);
+
+        expect(q.handleMessage).toHaveBeenCalledTimes(1);
+        expect(q.handleMessage).toHaveBeenCalledWith({
+          type: 'ewd-jwt-updateExpiry',
+          params: {
+            jwt: 'jwt-token',
+            application: 'login-micro-service'
+          }
+        }, jasmine.any(Function));
+      });
+
+      it('should call microservice client #send with correct arguments', function () {
+        /*jshint camelcase: false */
+        var microService = q.u_services.byDestination[destination];
+        /*jshint camelcase: true */
+
+        q.microServiceRouter(messageObj, handleResponse);
+
+        expect(microService.client.send).toHaveBeenCalledTimes(1);
+        expect(microService.client.send).toHaveBeenCalledWith({
+          application: 'login-micro-service',
+          type: 'restRequest',
+          path: '/api/users',
+          pathTemplate: '/path/template',
+          method: 'POST',
+          headers: {
+            'x-foo': 'bar'
+          },
+          params: {
+            type: 'foo'
+          },
+          query: {
+            bar: 'baz'
+          },
+          body: {
+            login: 'johndoe',
+            password: 'secret'
+          },
+          ip: '127.0.0.1',
+          ips: ['client'],
+          token: 'updated-jwt-token',
+          args: {},
+          jwt: true
+        }, jasmine.any(Function));
+      });
+
+      it('should call handleResponse', function () {
+        var responseObj = {
+          foo: 'bar'
+        };
+
+        /*jshint camelcase: false */
+        var microService = q.u_services.byDestination[destination];
+        /*jshint camelcase: true */
+
+        microService.client.send.and.callFake(function (message, cb) {
+          cb(responseObj);
+        });
+
+        q.microServiceRouter(messageObj, handleResponse);
+
+        expect(handleResponse).toHaveBeenCalledWith(responseObj);
+      });
+    },
+
     whenTokenReturned: function () {
       beforeEach(function (done) {
         boot(function (_q, _messageObj, _handleResponse) {
